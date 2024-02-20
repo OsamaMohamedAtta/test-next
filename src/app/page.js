d8837db746dc7c367624ed814aec7073741e8667
OsamaMohamedAtta <91071617+OsamaMohamedAtta@users.noreply.github.com>
2024-02-20 20:50:50 +0200

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,29 @@
 "use client"
-import styles from "./page.module.css";
-import React, { useState } from "react";
+import "./page.css";
+import React, { useState, useEffect } from "react";
 import 'mathlive';
 import '/node_modules/mathlive/dist/mathlive-static.css';
 import '/node_modules/mathlive/dist/mathlive-fonts.css';
 
 export default function Home() {
   const [value, setValue] = useState("");
+
+  useEffect(() => {
+    handleMathFailed()
+  }, [value]);
+
+  const handleMathFailed = () => {
+    document.querySelector('.layer').classList.replace('hide-layer', 'show-layer');
+    setTimeout(() => {
+      document.querySelector('.layer').classList.replace('show-layer', 'hide-layer');
+    }, 2000);
+  }
   return (
-    <main className={styles.main}>
+    <main className="main">
       <math-field onInput={evt => setValue(evt.target.value)} >
         {value}
       </math-field>
+      <div className="layer hide-layer"></div>
     </main>
   );
 }
