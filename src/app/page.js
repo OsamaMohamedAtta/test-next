a870f471b261412f1c3142a29e0ce4f2bdea67b2
OsamaMohamedAtta <91071617+OsamaMohamedAtta@users.noreply.github.com>
2024-02-20 18:23:33 +0200

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import styles from "./page.module.css";
 import React, { useState } from "react";
 import 'mathlive';
+import '/node_modules/mathlive/dist/mathlive-static.css';
+import '/node_modules/mathlive/dist/mathlive-fonts.css';
 
 export default function Home() {
   const [value, setValue] = useState("");
