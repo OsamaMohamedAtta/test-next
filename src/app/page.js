35fd085b59fa15bfdf6e0a0deb7c60001ce3c816
OsamaMohamedAtta <91071617+OsamaMohamedAtta@users.noreply.github.com>
2024-02-20 18:08:15 +0200

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import React, { useState } from "react";
+import 'mathlive';
 
 export default function Home() {
+  const [value, setValue] = useState("");
   return (
     <main className={styles.main}>
-      <h1>test next</h1>
+      <math-field onInput={evt => setValue(evt.target.value)} >
+        {value}
+      </math-field>
     </main>
   );
 }
