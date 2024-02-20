7d0d2d868e34454fc04802feee999bf0a8b5933b
OsamaMohamedAtta <91071617+OsamaMohamedAtta@users.noreply.github.com>
2024-02-20 20:09:42 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
 };
 
 module.exports = nextConfig
